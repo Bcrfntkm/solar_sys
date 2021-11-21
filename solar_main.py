fde5ee9856130f84790ea9c516963bfd28f62a57
Bcrfntkm <vasiutin.pa@phystech.edu>
2021-11-21 23:34:47 +0300

    Several graphs have been added.Final version

--- solar_main.py
+++ solar_main.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 # license: GPLv3
-
-import pygame as pg
+import numpy
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 import thorpy  # GUI for pygame
 from thorpy import functions
@@ -11,14 +11,14 @@ from thorpy import functions
 from solar_vis import *
 from solar_model import *
 from solar_input import *
-from solar_objects import *
 
 # timer_text = None
 alive = True
-perform_execution = False  # Флаг цикличности выполнения расчёта"""
-model_time = 0  # """Физическое время от начала расчёта.Тип: float"""
-time_scale = 3600.0  # """Шаг по времени при моделировании.  Тип: float"""
-space_objects = []  # """Список космических объектов."""
+perform_execution = False  # Флаг цикличности выполнения расчёта
+model_time = 0  # Физическое время от начала расчёта.Тип: float
+time_scale = 3600.0  # Шаг по времени при моделировании.  Тип: float
+space_objects = []  # Список космических объектов.
+
 # берём параметры экрана из модуля solar_vis
 width = window_width  # 1000
 height = window_height  # 900
@@ -30,6 +30,8 @@ drawer = None
 FPS = 60
 save_name = None
 
+
+# масса звезды для расчётов энергий
 
 def execution(delta):
     """Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
@@ -43,17 +45,15 @@ def execution(delta):
     model_time += delta
 
 
-# -----------------------------Pause
 def pause_execution():
+    """
+    Обработчик события нажатия на кнопку Start.Запускает циклическое исполнение функции execution.
+    """
     global perform_execution
     perform_execution = True
     start_execution()
 
 
-"""Обработчик события нажатия на кнопку Start.Запускает циклическое исполнение функции execution."""
-
-
-# -----------------------------Play/Pause
 def start_execution():
     global perform_execution
     global button_play
@@ -62,17 +62,18 @@ def start_execution():
         button_play.set_text('Pause')
     else:
         button_play.set_text('Play')
-    """ корректируем размер кнопки"""
+    # корректируем размер кнопки
     button_play.scale_to_title()
 
 
-# -----------------------------open file
-def stop_execution():  # """Обработчик события нажатия на кнопку Start.Останавливает циклическое исполнение функции execution."""
+def stop_execution():
+    """
+    Обработчик события нажатия на кнопку Start.Останавливает циклическое исполнение функции execution.
+    """
     global alive
     alive = False
 
 
-# -----------------------------open file
 def open_file():
     """Открывает диалоговое окно выбора имени файла и вызывает
     функцию считывания параметров системы небесных тел из данного файла.
@@ -81,25 +82,20 @@ def open_file():
     global space_objects
     global model_time
     global browserlauncher
-    #    global drawer
-    """ останавливаем выполнение на период загрузки"""
-    # pause_execution()
 
     in_filename = browserlauncher.var_text
     if in_filename != "":
-        # in_filename="solar_system.txt"
         model_time = 0.0
-        """ имя файла задано статически"""
-        # in_filename = "solar_system.txt"
-        """ создаём список(массив) объектов солнечной системы"""
+        # создаём список(массив) объектов солнечной системы
         space_objects = read_space_objects_data_from_file(in_filename)
-        """ определяем расстояние между самыми удалёнными объектами"""
+
+        # определяем расстояние между самыми удалёнными объектами
         max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
-        """ для корректного отображения на экране определяем соотношение между экраном и солнечной системой"""
+
+        # для корректного отображения на экране определяем соотношение между экраном и солнечной системой
         calculate_scale_factor(max_distance)
 
 
-# -----------------------------save file
 def save_file():
     """Открывает диалоговое окно выбора имени файла и вызывает
     функцию считывания параметров системы небесных тел из данного файла.
@@ -109,16 +105,16 @@ def save_file():
     global model_time
     global browserlauncher
     global save_name
-    """останавливаем выполнение на период загрузки"""
+
+    # останавливаем выполнение на период загрузки
     pause_execution()
     out_filename = save_name.get_value()
     print(out_filename)
     if out_filename != "":
-        """ создаём список(массив) объектов солнечной системы"""
+        # создаём список(массив) объектов солнечной системы
         write_space_objects_data_to_file(out_filename, space_objects)
 
 
-# ---------------------------------------------handle events
 def handle_events(events, menu):
     global alive
     for event in events:
@@ -128,7 +124,6 @@ def handle_events(events, menu):
             alive = False
 
 
-# -----------------------------slider reaction
 def slider_to_real(val):
     return np.exp(5 + val)
 
@@ -138,7 +133,6 @@ def slider_reaction(event):
     time_scale = slider_to_real(event.el.get_value())
 
 
-# -----------------------------init menu
 def init_ui(screen):
     global button_play
     global browserlauncher
@@ -146,7 +140,8 @@ def init_ui(screen):
 
     slider = thorpy.SliderX(100, (-10, 10), "Simulation speed")
     slider.user_func = slider_reaction
-    """ создаём кнопки и привязываем их к процедурам"""
+
+    # создаём кнопки и привязываем их к процедурам
     button_stop = thorpy.make_button('Quit', func=stop_execution)
     button_pause = thorpy.make_button('Pause', func=pause_execution)
     button_play = thorpy.make_button('Play', func=start_execution)
@@ -154,7 +149,7 @@ def init_ui(screen):
     button_save = thorpy.make_button(text='save a file', func=save_file)
 
     browser = thorpy.Browser(file_types=['.txt'], text='Browser', folders=True)
-    browserlauncher = thorpy.BrowserLauncher(browser, const_text='file:')
+    browserlauncher = thorpy.BrowserLauncher(browser, const_text='file:', var_text="solar_system.txt")
     browserlauncher.max_chars = 40  # limit size of browser launcher
     browserlauncher.scale_to_title()
 
@@ -168,7 +163,7 @@ def init_ui(screen):
         save_name,
         button_save,
         button_stop,
-        #    button_pause,
+        # button_pause,
         timer_text])
 
     # фильтр событий для слайдера
@@ -184,18 +179,14 @@ def init_ui(screen):
         element.surface = screen
 
     box.set_topleft((0, 0))
-    ##    box.blit()
-    ##    box.update()
     functions.set_current_menu(menu)
     return menu, box, timer_text
 
 
-# ------------------------------------------------------------main
 def main():
     """Главная функция главного модуля.
     Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
     """
-
     global physical_time
     global displayed_time
     global time_step
@@ -207,7 +198,6 @@ def main():
     global screen
     global menu
     global drawer
-
     print('Modelling started!')
     physical_time = 0
 
@@ -233,12 +223,32 @@ def main():
             text = "%d days passed" % (int(model_time / 3600 / 24))
             timer_text.set_text(text)
         last_time = cur_time
+        # задержка до начала следующего фрейма
         clock.tick(FPS)
         drawer.update(space_objects, menu)  # vis.py
         # задержка на 1/60 секунды
-    #        time.sleep(1.0 / 60)
-    # задержка до начала следующего фрейма
+        # time.sleep(1.0 / 60)
 
+    if len(space_objects):
+        # выводим графики. массив body.rV и body.En
+        fig, (ax_en, ax_rv) = plt.subplots(2, 1)
+        # количество отсчетов по оси x
+        x_size = np.size(space_objects[0].obj.En)
+        # print(x_size)
+        x = np.linspace(0, x_size - 1, x_size)
+        i = 0
+        for body in space_objects:
+            # переводим исходные массивы в массивы numpy
+            y = numpy.array(body.obj.En)
+            ax_en.plot(x, y, label=str(i))
+            y = numpy.array(body.obj.rV)
+            ax_rv.plot(x, y, label=str(i))
+            i += 1
+        ax_en.set_title('Energy')
+        ax_en.legend()
+        ax_rv.set_title('Angular momentum')
+        ax_rv.legend()
+        plt.show()
     print('Modelling finished!')
     pg.quit()
 
